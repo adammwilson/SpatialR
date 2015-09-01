05ad48564dce3b48ccf4075a9d33765681d92e3d
Adam M. Wilson <adammichaelwilson@gmail.com>
2015-09-01 12:28:45 -0400

    Add lecture 2

---
+++ 02_DataGymnastics/02_DataGymnastics.R
@@ -0,0 +1,46 @@
+#' ---
+#' title: "Data Wrangling"
+#' author: "Adam M. Wilson"
+#' date: "August 31, 2015"
+#' output:
+#'   slidy_presentation:
+#'     fig_width: 7
+#'     fig_height: 6
+#'     keep_md: true
+#' ---
+#'   
+#' 
+#' ## From last week
+#' Syllabus:
+#' Added grade translations
+#' UBLearns grade center
+#' 
+#' 
+#' 
+#' ## RStudio Shortcuts
+#' ctrl-R to run line or selection
+#' source
+#' 
+#' ## RMarkdown presentation
+#' Last week shared only .R
+#' Will share .Rmd now
+#' 
+#' 
+#' 
+#' ## Data wrangling
+#' 
+#' `dplyr` 
+#' `tidyr`
+#' 
+#' [Cheat sheets on website](https://www.rstudio.com/resources/cheatsheets/)
+#' [Data Wrangling](https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf)
+#' 
+#' 
+#' spatial data operations?
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
---
+++ 02_DataGymnastics/02_DataGymnastics.md
@@ -0,0 +1,40 @@
+# Data Wrangling
+Adam M. Wilson  
+August 31, 2015  
+  
+
+
+## From last week
+Syllabus:
+Added grade translations
+UBLearns grade center
+
+
+
+## RStudio Shortcuts
+ctrl-R to run line or selection
+source
+
+## RMarkdown presentation
+Last week shared only .R
+Will share .Rmd now
+
+
+
+## Data wrangling
+
+`dplyr` 
+`tidyr`
+
+[Cheat sheets on website](https://www.rstudio.com/resources/cheatsheets/)
+[Data Wrangling](https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf)
+
+
+spatial data operations?
+
+
+
+
+
+
+
