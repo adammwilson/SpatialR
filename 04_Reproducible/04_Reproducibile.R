bce1fdee9cfaafe1369dcd65c7b60439fec6e090
Adam M. Wilson <adammichaelwilson@gmail.com>
2015-09-28 16:40:43 -0400

    final updates

--- 04_Reproducible/04_Reproducibile.R
+++ 04_Reproducible/04_Reproducibile.R
@@ -26,40 +26,40 @@
 #' 
 #' * Got Git?
 #' 
-#' # Homework Review
+#' ## Homework Review
 #' 
-#' #  Today
+#' ##  Today
 #' 
-#' ## Outline
-#' * More Git
-#' * More Markdown
-#' * Introduction to `ggplot2()`
-#' * Introduction to spatial data in R
+#' ### Outline
+#' 
+#'   * Project Proposal
+#'   * More Git
+#'   * More Markdown
+#'   
+#' **Guided Exploration**
+#' 
+#'   * Reading files
+#'   * Introduction to `ggplot2()`
+#' 
+#' ## Project proposal
+#' 
+#' **Final project:** 
+#' 
+#' dynamically generated report of a spatial analysis (minimum 12 page, maximum 30 pages).  
+#' 
+#' **Project proposal:**
+#' 
+#' <1 page due next week
+#' 
+#' 1. Introduction to problem/question
+#' 2. Proposed data sources
+#' 3. Proposed methods
+#' 4. Expected results
+#' 
+#' 
 #' 
 #' # Working with Git and GitHub
 #' 
-#' ## Git Has Integrity
-#' Everything _checksummed_ before storage and then referred by _checksum_. 
-#' 
-#' > It’s impossible to change the contents of any file or directory without Git knowing. You can’t lose information in transit or get file corruption without Git being able to detect it.
-#' 
-#' A 40-character hexadecimal SHA-1 hash:
-#' 
-#' `24b9da6552252987aa493b52f8696cd6d3b00373`
-#' 
-#' ## Checksum
-#' A way of reducing digital information to a unique ID:
-#' 
-#' <img src="assets/checksum.jpg" alt="alt text" width="50%">
-#' 
-#' Git doesn't care about filenames, extensions, etc.  It' the information that matters...
-#' 
-#' ## The 3 states of files
-#' 
-#' ### staged, modified, committed
-#' <img src="assets/staging.png" alt="alt text" width="75%">
-#' 
-#' The important stuff is hidden in the `.git` folder.
 #' 
 #' ## Commit to GitHub from within RStudio
 #' 
@@ -68,6 +68,14 @@
 #' 1. Stage
 #' 2. Commit (with a message)
 #' 3. Push
+#' 
+#' ## The 3 states of files
+#' 
+#' ### staged, modified, committed
+#' <img src="assets/staging.png" alt="alt text" width="75%">
+#' 
+#' The important stuff is hidden in the `.git` folder.
+#' 
 #' 
 #' 
 #' ## Staging
@@ -140,6 +148,23 @@
 #' ```
 #' But we won't do much with branching in this course...
 #' 
+#' ## Git Has Integrity
+#' Everything _checksummed_ before storage and then referred by _checksum_. 
+#' 
+#' > It’s impossible to change the contents of any file or directory without Git knowing. You can’t lose information in transit or get file corruption without Git being able to detect it.
+#' 
+#' A 40-character hexadecimal SHA-1 hash:
+#' 
+#' `24b9da6552252987aa493b52f8696cd6d3b00373`
+#' 
+#' ## Checksum
+#' A way of reducing digital information to a unique ID:
+#' 
+#' <img src="assets/checksum.jpg" alt="alt text" width="50%">
+#' 
+#' Git doesn't care about filenames, extensions, etc.  It' the information that matters...
+#' 
+#' 
 #' ## Git can do far more!
 #' 
 #' Check out the (free) book [ProGIT](https://git-scm.com/book/en/v2)
@@ -153,6 +178,8 @@
 #' Remember, the data and code are _real_, the products (tables, figures) are ephemeral...  
 #' 
 #' # RMarkdown
+#' 
+#' ## RMarkdown: workflow
 #' <img src="assets/Rmarkdown01.png" alt="alt text" width="100%">
 #' 
 #' ## RMarkdown: new file
@@ -241,7 +268,7 @@ plot(cars)
 #' 
 #' <br>
 #' 
-#' ### Take 10 minutes & ask questions!
+#' ### Take 15 minutes & ask questions!
 #' 
 #' 
 #' ## Colophon
